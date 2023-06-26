c30b9d789f3478d4b6138e6098cd17e4ffb66df8
angelaantonia.cea <angelaantonia.cea@dxc.com>
2023-06-26 16:17:42 +0200

    Fixed junit test due to x-ray implementation

--- src/test/java/it/pagopa/interop/probing/telemetry/writer/unit/consumer/TelemetryReceiverTest.java
+++ src/test/java/it/pagopa/interop/probing/telemetry/writer/unit/consumer/TelemetryReceiverTest.java
@@ -3,17 +3,21 @@ package it.pagopa.interop.probing.telemetry.writer.unit.consumer;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.io.IOException;
-import javax.validation.ConstraintViolationException;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.xray.AWSXRay;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.awspring.cloud.messaging.listener.SimpleMessageListenerContainer;
 import it.pagopa.interop.probing.telemetry.writer.consumer.TelemetryReceiver;
@@ -28,7 +32,7 @@ class TelemetryReceiverTest {
   @Autowired
   private TelemetryReceiver telemetryReceiver;
 
-  @Mock
+  @MockBean
   private TimestreamService timestreamService;
 
   @MockBean
@@ -37,24 +41,37 @@ class TelemetryReceiverTest {
   @Autowired
   private ObjectMapper mapper;
 
+  private Map<String, String> attributes = new HashMap<>();
+  private String mockedId = "mockedId";
+  Message message = new Message();
+
+  @BeforeEach
+  void setup() {
+    AWSXRay.beginSegment("Segment-test");
+    attributes.put("AWSTraceHeader", mockedId);
+  }
+
+  @AfterEach
+  void clean() {
+    AWSXRay.endSegment();
+  }
 
   @Test
   @DisplayName("The receiveMessage method of TelemetryReceiver handles valid message successfully.")
   void testReceiveMessage_thenDoesNotThrowException() throws IOException {
-    String message =
+    String messageString =
         "{\"eserviceRecordId\":3,\"status\":\"KO\",\"responseTime\":null,\"koReason\":\"exception\",\"checkTime\":\"1683023923118\"}";
+    message.setBody(messageString);
+    message.setAttributes(attributes);
     Mockito.doNothing().when(timestreamService)
-        .writeRecord(mapper.readValue(message, TelemetryDto.class));
+        .writeRecord(mapper.readValue(messageString, TelemetryDto.class));
     assertDoesNotThrow(() -> telemetryReceiver.receiveMessage(message));
   }
 
   @Test
   @DisplayName("The receiveMessage method of TelemetryReceiver throws a ConstraintViolationException in case of an invalid message.")
   void testReceiveMessage_thenDoesThrowValidationException() throws IOException {
-    String message =
-        "{\"eserviceRecordId\":3,\"status\":\"OK\",\"responseTime\":null,\"koReason\":\"exception\",\"checkTime\":\"1683023923118\"}";
-    assertThrows(ConstraintViolationException.class,
-        () -> telemetryReceiver.receiveMessage(message));
+    assertThrows(IllegalArgumentException.class, () -> telemetryReceiver.receiveMessage(message));
   }
 
 
